8e307680f967dbad9b326b3e45a5285753e2546f
shogun <shoguncrypto93@gmail.com>
2025-03-10 14:46:52 +0700

    [ft] add tracing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
 use sqlx::postgres::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // LogTracer::init().expect("Failed to set logger");
+    //
+    // let env_filter = EnvFilter::try_from_default_env()
+    //     .unwrap_or_else(|_| EnvFilter::new("info"));
+    // let formatting_layer = BunyanFormattingLayer::new(
+    //     "zero2prod".into(),
+    //     std::io::stdout
+    // );
+    // let subscriber = Registry::default()
+    //     .with(env_filter)
+    //     .with(JsonStorageLayer)
+    //     .with(formatting_layer);
+    // set_global_default(subscriber).expect("Failed to set subscriber");
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -3,6 +3,22 @@ use std::net::TcpListener;
 use uuid::Uuid;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+
+use once_cell::sync::Lazy;
+
+// Ensure that the `tracing` stack is only initialised once using `once_cell`
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    };
+});
 
 pub struct TestApp {
     pub address: String,
@@ -10,6 +26,10 @@ pub struct TestApp {
 }
 
 async fn spawn_app() -> TestApp {
+    // let subscriber = get_subscriber("test".into(), "debug".into());
+    // init_subscriber(subscriber);
+    Lazy::force(&TRACING);
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     // We retrieve the port assigned to us by the OS
     let port = listener.local_addr().unwrap().port();
