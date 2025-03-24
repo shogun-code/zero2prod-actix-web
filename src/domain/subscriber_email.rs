a99ade18f706cd9ff1c3affb7eedfcab00863daa
shogun <shoguncrypto93@gmail.com>
2025-03-24 17:21:05 +0700

    [ft] delivery newsletter to all subscibers

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -3,6 +3,14 @@ use validator::ValidateEmail;
 #[derive(Debug)]
 pub struct SubscriberEmail(String);
 
+impl std::fmt::Display for SubscriberEmail {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        // We just forward to the Display implementation of
+        // the wrapped String.
+        self.0.fmt(f)
+    }
+}
+
 impl SubscriberEmail {
     pub fn parse(s: String) -> Result<SubscriberEmail, String> {
         if s.validate_email() {
